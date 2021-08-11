0e016a0a8b7d3ff18bc182093386cde79be61ea8
Tom <tom@contextfree.info>
2021-08-10 22:06:22 -0700

    Use core; work more

--- src/tacana.zig
+++ src/tacana.zig
@@ -3,36 +3,38 @@ const std = @import("std");
 
 const assert = std.debug.assert;
 
-const SomeError = error {
-    Error,
-};
-
 pub fn main() !void {
     // Init
     var engine = c.wasm_engine_new() orelse unreachable;
     defer c.wasm_engine_delete(engine);
-    var store = c.wasmtime_store_new(engine, null, null) orelse unreachable;
-    defer c.wasmtime_store_delete(store);
-    var context = c.wasmtime_store_context(store);
-    // Load
+    var store = c.wasm_store_new(engine) orelse unreachable;
+    defer c.wasm_store_delete(store);
+    // var context = c.wasmtime_store_context(store);
+    // Load wat and wasm
     const wat = @embedFile("hello.wat");
     var wasm: c.wasm_byte_vec_t = undefined;
     var err = c.wasmtime_wat2wasm(wat, wat.len, &wasm);
-    if (err != null) {
-        std.log.err("failed to parse wat", .{});
-        return SomeError.Error;
-    }
-    // TODO Coordinate early delete of wasm.
-    defer c.wasm_byte_vec_delete(&wasm);
+    assert(err == null);
     // Compile
     std.log.info("Compiling module ...", .{});
-    var module: ?*c.wasmtime_module_t = null;
-    err = c.wasmtime_module_new(engine, wasm.data, wasm.size, &module);
-    if (err != null) {
-        std.log.err("failed to compile module", .{});
-        return SomeError.Error;
-    }
-    // defer c.wasm_module_delete(@ptrCast(*c.wasm_module_t, module));
+    var module = c.wasm_module_new(store, &wasm) orelse unreachable;
+    defer c.wasm_module_delete(module);
+    c.wasm_byte_vec_delete(&wasm);
+    // Register
+    std.log.info("Instantiating module ...", .{});
+    var hello_type = c.wasm_functype_new_0_0();
+    var hello_func = c.wasm_func_new(store, hello_type, helloCallback);
+    c.wasm_functype_delete(hello_type);
+    // Instantiate
+}
+
+fn helloCallback(
+    args: [*c]const c.wasm_val_vec_t,
+    results: [*c]c.wasm_val_vec_t,
+) callconv(.C) ?*c.wasm_trap_t {
+    std.debug.print("Calling back...\n", .{});
+    std.debug.print("> Hello World!\n", .{});
+    return null;
 }
 
 // fn exit_with_error(const char *message, wasmtime_error_t *error, wasm_trap_t *trap) !void {
