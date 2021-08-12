9315ea2823d932aefd54d1a2329649959c6c51c2
Tom <tom@contextfree.info>
2021-08-11 21:18:09 -0700

    Call that function

--- src/tacana.zig
+++ src/tacana.zig
@@ -4,11 +4,13 @@ const std = @import("std");
 const assert = std.debug.assert;
 
 pub fn main() !void {
+    defer std.log.info("Done.", .{});
     // Init
     var engine = c.wasm_engine_new() orelse unreachable;
     defer c.wasm_engine_delete(engine);
     var store = c.wasm_store_new(engine) orelse unreachable;
     defer c.wasm_store_delete(store);
+    defer std.log.info("Shutting down ...", .{});
     // var context = c.wasmtime_store_context(store);
     // Load wat and wasm
     const wat = @embedFile("hello.wat");
@@ -18,7 +20,7 @@ pub fn main() !void {
     // Compile
     std.log.info("Compiling module ...", .{});
     var module = c.wasm_module_new(store, &wasm) orelse unreachable;
-    defer c.wasm_module_delete(module);
+    // defer c.wasm_module_delete(module);
     c.wasm_byte_vec_delete(&wasm);
     // Register
     std.log.info("Creating callback ...", .{});
@@ -31,6 +33,21 @@ pub fn main() !void {
     var imports = c.wasm_extern_vec_t{ .size = externs.len, .data = &externs };
     var instance = c.wasm_instance_new(store, module, &imports, null) orelse unreachable;
     c.wasm_func_delete(hello_func);
+    // Extract
+    std.log.info("Extracting export ...", .{});
+    var exports: c.wasm_extern_vec_t = undefined;
+    c.wasm_instance_exports(instance, &exports);
+    assert(exports.size > 0);
+    var run_func = c.wasm_extern_as_func(exports.data[0]) orelse unreachable;
+    c.wasm_instance_delete(instance);
+    c.wasm_module_delete(module);
+    // Call
+    std.log.info("Calling export ...", .{});
+    var args = c.wasm_val_vec_t{ .size = 0, .data = null };
+    var results = c.wasm_val_vec_t{ .size = 0, .data = null };
+    var trap = c.wasm_func_call(run_func, &args, &results);
+    assert(trap == null);
+    c.wasm_extern_vec_delete(&exports);
 }
 
 fn helloCallback(
@@ -41,18 +58,3 @@ fn helloCallback(
     std.debug.print("> Hello World!\n", .{});
     return null;
 }
-
-// fn exit_with_error(const char *message, wasmtime_error_t *error, wasm_trap_t *trap) !void {
-//   fprintf(stderr, "error: %s\n", message);
-//   wasm_byte_vec_t error_message;
-//   if (error != NULL) {
-//     wasmtime_error_message(error, &error_message);
-//     wasmtime_error_delete(error);
-//   } else {
-//     wasm_trap_message(trap, &error_message);
-//     wasm_trap_delete(trap);
-//   }
-//   fprintf(stderr, "%.*s\n", (int) error_message.size, error_message.data);
-//   wasm_byte_vec_delete(&error_message);
-//   exit(1);
-// }
