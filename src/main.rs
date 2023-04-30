e2e8df562e41a16755c79f58d149252e26b1a2e9
Tom <tom@contextfree.info>
2023-04-30 08:19:06 -0700

    Try callbacks

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
-use wasmer::{Store, Module, Instance, Value, imports};
+use wasmer::{imports, Function, Instance, Module, Store, Value};
 
 fn main() -> anyhow::Result<()> {
     let module_wat = r#"
-    (module
-      (type $t0 (func (param i32) (result i32)))
-      (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
-        get_local $p0
-        i32.const 1
-        i32.add))
+        (import "host" "hello" (func $host_hello (param i32)))
+
+        (func (export "hello") (param $n i32) (result i32)
+            (local.set $n (i32.add (local.get $n) (i32.const 1)))
+            (call $host_hello (local.get $n))
+            local.get $n
+        )
     "#;
 
     let mut store = Store::default();
     let module = Module::new(&store, &module_wat)?;
-    // The module doesn't import anything, so we create an empty import object.
-    let import_object = imports! {};
+    let import_object = imports! {
+        "host" => {
+            "hello" => Function::new_typed(&mut store, |n: i32| {
+                println!("Hello {n}");
+            }),
+        }
+    };
     let instance = Instance::new(&mut store, &module, &import_object)?;
 
-    let add_one = instance.exports.get_function("add_one")?;
+    let add_one = instance.exports.get_function("hello")?;
     let result = add_one.call(&mut store, &[Value::I32(42)])?;
     assert_eq!(result[0], Value::I32(43));
 
