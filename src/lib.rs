21616545a7e44573f205c5e7fd4600a4bcb4836f
Tom <tom@contextfree.info>
2023-05-06 13:26:29 -0700

    Add wgpu-native; always downlevel for now

--- src/lib.rs
+++ src/lib.rs
@@ -41,13 +41,7 @@ impl State {
                 &wgpu::DeviceDescriptor {
                     label: None,
                     features: wgpu::Features::empty(),
-                    // WebGL doesn't support all of wgpu's features, so if
-                    // we're building for the web we'll have to disable some.
-                    limits: if cfg!(target_arch = "wasm32") {
-                        wgpu::Limits::downlevel_webgl2_defaults()
-                    } else {
-                        wgpu::Limits::default()
-                    },
+                    limits: wgpu::Limits::downlevel_webgl2_defaults(),
                 },
                 // Some(&std::path::Path::new("trace")), // Trace path
                 None,
