907b6fc8d67f691de27dcf77c83b278b76393f9a
Tom <tom@contextfree.info>
2023-08-19 07:42:18 -0700

    Call windowListen in nelua

---
+++ examples/nelua/run.sh
@@ -0,0 +1,6 @@
+CC=$WASI_SDK_PATH/bin/clang
+nelua --cc="$CC" --release main.nelua --output main.wasm && \
+    wasm2wat --generate-names main.wasm -o main.wat && \
+    ../../target/release/taca run main.wasm
+
+# nelua --cc="$CC" main.nelua --output main.wasm
