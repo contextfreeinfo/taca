bacb650681a436d8a9a1ebe8a7a9c5e1ebc53908
Tom <tom@contextfree.info>
2023-08-19 07:43:44 -0700

    Shrink and watch size

--- examples/nelua/run.sh
+++ examples/nelua/run.sh
@@ -1,6 +1,7 @@
 CC=$WASI_SDK_PATH/bin/clang
 nelua --cc="$CC" --release main.nelua --output main.wasm && \
     wasm2wat --generate-names main.wasm -o main.wat && \
+    ls -l && \
     ../../target/release/taca run main.wasm
 
 # nelua --cc="$CC" main.nelua --output main.wasm
