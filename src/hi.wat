6cbc2502621a7f8fd9367bfacf59d58154f72c98
Tom <tom@contextfree.info>
2024-06-08 18:59:56 -0700

    Explore some

--- src/hi.wat
+++ src/hi.wat
@@ -1,4 +1,10 @@
 (module
+  (type $t0 (func (param i32) (result i32)))
   (func $hi (import "" "hi"))
   (func (export "run") (call $hi))
+  (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
+    get_local $p0
+    i32.const 1
+    i32.add
+  )
 )
--- src/wasmic.rs
+++ src/wasmic.rs
@@ -1,23 +1,31 @@
+use wasmer::Value;
+
 pub fn wasmish() -> anyhow::Result<()> {
-    #[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
+    #[cfg(not(target_arch = "wasm32"))]
     {
         use wasmer::{imports, Function, Instance, Module, Store};
         let module_wat = include_str!("hi.wat");
 
         let mut store = Store::default();
         let module = Module::new(&store, &module_wat)?;
-        // The module doesn't import anything, so we create an empty import object.
         let import_object = imports! {
             "" => {
-                 "hi" => Function::new_typed(&mut store, || println!("Hello")),
+                 "hi" => Function::new_typed(&mut store, hi),
             }
         };
         let instance = Instance::new(&mut store, &module, &import_object)?;
-    
-        let add_one = instance.exports.get_function("run")?;
-        add_one.call(&mut store, &[])?;
-        // assert_eq!(result[0], Value::I32(43));
+
+        let run = instance.exports.get_function("run")?;
+        run.call(&mut store, &[])?;
+
+        let add_one = instance.exports.get_function("add_one")?;
+        let result = add_one.call(&mut store, &[Value::I32(42)])?;
+        assert_eq!(result[0], Value::I32(43));
     }
 
     Ok(())
+}
+
+fn hi() {
+    println!("Hi!");
 }
