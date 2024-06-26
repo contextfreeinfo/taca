c9fa81aa4d31901112f1bd71448b3a9063c3acc2
Tom <tom@contextfree.info>
2024-06-26 05:07:35 -0700

    Load title

--- cana/src/main.ts
+++ cana/src/main.ts
@@ -1,24 +1,110 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// import "./style.css";
+// import logo from "../../taca-logo.svg";
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${logo}" class="logo" alt="Vite logo" />
+//     </a>
+//   </div>
+// `;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+async function main() {
+  await loadApp();
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+/** Allows for additional custom properties. */
+class App {
+  config: (() => void) | undefined;
+
+  init(instance: WebAssembly.Instance) {
+    Object.assign(this, instance.exports as any);
+  }
+
+  listen: ((event: number) => void) | undefined;
+
+  memory!: WebAssembly.Memory;
+
+  #memoryBuffer: ArrayBuffer | undefined;
+  #memoryBufferBytes: Uint8Array | undefined;
+
+  memoryBytes() {
+    if (this.#memoryBuffer != this.memory.buffer) {
+      // Either on first access or on internal reallocation.
+      this.#memoryBuffer = this.memory.buffer;
+      this.#memoryBufferBytes = new Uint8Array(this.#memoryBuffer);
+    }
+    return this.#memoryBufferBytes!;
+  }
+
+  readString(spanPtr: number) {
+    // Can cache memory bytes when no app calls are being made.
+    const memoryBytes = this.memoryBytes();
+    const spanView = new DataView(memoryBytes.buffer, spanPtr, 2 * 4);
+    // Wasm is explicitly little-endian.
+    const stringPtr = spanView.getUint32(0, true);
+    const stringLen = spanView.getUint32(4, true);
+    const chunk = memoryBytes.slice(stringPtr, stringPtr + stringLen);
+    return new TextDecoder("utf-8").decode(chunk);
+  }
+
+  start!: () => void;
+}
+
+async function loadApp() {
+  const appData = await loadAppData();
+  if (!appData) return;
+  let app = new App();
+  const env = makeAppEnv(app);
+  let { instance } = await WebAssembly.instantiate(appData, { env });
+  app.init(instance);
+  // TODO Fold config into start once we get fully off miniquad.
+  if (app.config) {
+    app.config();
+  }
+  app.start();
+}
+
+async function loadAppData() {
+  const url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search);
+  const app = params.get("app");
+  if (app) {
+    return await (await fetch(app)).arrayBuffer();
+  }
+}
+
+function makeAppEnv(app: App) {
+  return {
+    taca_RenderingContext_applyBindings(context: number, bindings: number) {},
+    taca_RenderingContext_applyPipeline(context: number, pipeline: number) {},
+    taca_RenderingContext_applyUniforms(context: number, uniforms: number) {},
+    taca_RenderingContext_beginPass(context: number) {},
+    taca_RenderingContext_commitFrame(context: number) {},
+    taca_RenderingContext_draw(
+      context: number,
+      item_begin: number,
+      item_count: number,
+      instance_count: number
+    ) {},
+    taca_RenderingContext_endPass(context: number) {},
+    taca_RenderingContext_newBuffer(
+      context: number,
+      typ: number,
+      usage: number,
+      info: number
+    ) {},
+    taca_RenderingContext_newPipeline(context: number, bytes: number) {},
+    taca_RenderingContext_newShader(context: number, bytes: number) {},
+    taca_Window_newRenderingContext() {},
+    taca_Window_print(text: number) {},
+    taca_Window_setTitle(title: number) {
+      // TODO Abstract to provide callbacks for these things?
+      document.title = app.readString(title);
+    },
+    taca_Window_state(result: number) {},
+  };
+}
+
+const textDecoder = new TextDecoder();
+
+main();
