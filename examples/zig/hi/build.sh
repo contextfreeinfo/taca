694e9b359a9ac2f84bad62bc9d6fc01ddd7cf570
Tom <tom@contextfree.info>
2024-08-08 07:11:59 -0700

    Fix workflow (#10)
    * Try harder to convince lz4
    * See about rustup
    * Explore more
    * Change rustup root
    * Provide git; avoid dev post-build
    * Change permissions for peaceiris
    * Try standard actions
    * Put in docs

--- examples/zig/hi/build.sh
+++ examples/zig/hi/build.sh
@@ -3,7 +3,7 @@ build() {
     spirv-opt -Os src/shader.spv -o src/shader.opt.spv && \
     zig build && \
     wasm-opt -Os zig-out/bin/hi.wasm -o zig-out/bin/hi.opt.wasm && \
-    lz4 -f9 zig-out/bin/hi.opt.wasm
+    lz4 -f9 zig-out/bin/hi.opt.wasm zig-out/bin/hi.opt.wasm.lz4
 }
 
 place() {
