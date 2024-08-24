3c0033e6c29d85c7152f5942254dde2a5b9a3759
Tom <tom@contextfree.info>
2024-08-24 16:33:50 -0700

    Put spv in build

--- examples/c3/fly/build.sh
+++ examples/c3/fly/build.sh
@@ -1,7 +1,7 @@
 PUB_DIR=../../../web/public/apps/c3
 
-dxc -T vs_6_0 -E vertex_main -spirv -Fo src/vertex.spv src/shader.hlsl && \
-dxc -T ps_6_0 -E fragment_main -spirv -Fo src/fragment.spv src/shader.hlsl && \
+dxc -T vs_6_0 -E vertex_main -spirv -Fo build/vertex.spv src/shader.hlsl && \
+dxc -T ps_6_0 -E fragment_main -spirv -Fo build/fragment.spv src/shader.hlsl && \
 c3c compile --reloc=none --target wasm32 -g0 --link-libc=no --no-entry -Os \
     src/*.c3 --output-dir build && \
 wasm-opt -Os build/out.wasm -o build/out.opt.wasm && \
