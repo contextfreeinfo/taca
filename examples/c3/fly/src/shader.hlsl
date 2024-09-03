a83b08a353fea08574d0d5589cc9efd7da964834
Tom <tom@contextfree.info>
2024-09-02 21:32:29 -0700

    Rotate around center

--- examples/c3/fly/src/shader.hlsl
+++ examples/c3/fly/src/shader.hlsl
@@ -11,20 +11,34 @@ struct VSInput {
 
 struct PSInput {
     float4 pos : SV_POSITION;
-    float norm : TEXCOORD0;
+    float bright : TEXCOORD0;
 };
+
+#define PI 3.14159265359
 
 PSInput vertex_main(VSInput input) {
     PSInput output;
-    float4 pos_world = float4(input.pos + input.offset, 1);
+    // Always rotate original +y toward the center.
+    float angle = atan2(input.offset.y, input.offset.x) + PI / 2;
+    float c = cos(angle);
+    float s = sin(angle);
+    float3x3 rot = float3x3(
+        c, -s, 0,
+        s, c, 0,
+        0, 0, 1
+    );
+    float3 pos = mul(rot, input.pos) + input.offset;
+    float4 pos_world = float4(pos, 1);
     float4 pos_proj = mul(proj, pos_world);
     output.pos = mul(view, pos_proj);
-    output.norm = dot(input.norm, normalize(float3(1, 1, 0)));
-    output.norm = 0.5 * output.norm + 0.5;
+    // Also rotate norm.
+    float3 norm = mul(rot, input.norm);
+    float bright = dot(norm, normalize(float3(1, 1, 0)));
+    output.bright = 0.5 * bright + 0.5;
     return output;
 }
 
 float4 fragment_main(PSInput input) : SV_TARGET {
-    float3 color = (float3(0, 1, 0) * input.norm) * 0.5 + 0.25;
+    float3 color = (float3(0, 1, 0) * input.bright) * 0.5 + 0.25;
     return float4(color, 1);
 }
