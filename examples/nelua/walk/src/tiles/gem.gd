77e0377110e852bc763b6b8a7039727755d80e21
Tom <tom@contextfree.info>
2024-09-17 05:10:07 -0700

    Add resources; adjust compiler flags

---
+++ examples/nelua/walk/src/tiles/gem.gd
@@ -0,0 +1,11 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	autoplay = "shine"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
