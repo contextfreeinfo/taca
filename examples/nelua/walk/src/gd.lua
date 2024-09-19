c510d10e5d013fd91abe46bdcf0941f25e364204
Tom <tom@contextfree.info>
2024-09-19 06:52:09 -0700

    Parse attrs & ids

--- examples/nelua/walk/src/gd.lua
+++ examples/nelua/walk/src/gd.lua
@@ -1,7 +1,7 @@
 local gd = {}
 
 local parse_value
-local section_pattern = "^%[(.+)%]$"
+local header_pattern = "^%[(.+)%]$"
 
 local function trim(s)
   return s:match("^[,%s]*(.-)[,%s]*$")
@@ -18,7 +18,7 @@ local function read_line(file, text)
       return nil
     end
     line = trim(line)
-    if not (line:match("^%s*$") or line:match("^%s*;") )then
+    if not (line:match("^%s*$") or line:match("^%s*;")) then
       return line
     end
   end
@@ -49,6 +49,7 @@ end
 
 local function parse_call(file, type, args)
   local values = {}
+  -- This splits on spaces in strings. TODO Parse like arrays.
   for match in args:gmatch("[^,%s]+") do
     local value = parse_value(file, match)
     table.insert(values, value)
@@ -87,13 +88,13 @@ parse_value = function(file, text)
   if num then
     return tonumber(num), rest
   end
-  -- String. Presume no quotes inside strings for now.
+  -- String. Presume no quotes nor escapes inside strings for now.
   local str, rest = text:match("^&?\"(.-)\"(.*)")
   if str then
     return str, rest
   end
-  -- Call.
-  local type, args, rest = text:match("^(%w+)%((.-)%)(.*)")
+  -- Call. Presume only numbers & paren-less strings on single line for now.
+  local type, args, rest = text:match("^([%w_]+)%((.-)%)(.*)")
   if type then
     return parse_call(file, type, args), rest
   end
@@ -128,7 +129,7 @@ local function parse_section(file)
     if not line then
       break
     end
-    if line:match(section_pattern) then
+    if line:match(header_pattern) then
       file:seek("cur", - #line - 1)
       break
     end
@@ -141,16 +142,26 @@ local function parse_section(file)
   return section_data
 end
 
+local function parse_header(header)
+  local attrs = {}
+  local tag, rest = header:match("^([%w_]+)%s*(.*)")
+  -- Presume no nested quotes for now.
+  for key, value in rest:gmatch("([%w_]+)%s*=%s*\"(.-)\"") do
+    attrs[key] = value
+  end
+  return tag, attrs
+end
+
 local function print_table(t, indent)
   indent = indent or ""
   for k, v in pairs(t) do
-      if type(v) == "table" then
-          print(indent .. k .. " = {")
-          print_table(v, indent .. "  ")
-          print(indent .. "}")
-      else
-          print(indent .. k .. " = " .. tostring(v))
-      end
+    if type(v) == "table" then
+      print(indent .. k .. " = {")
+      print_table(v, indent .. "  ")
+      print(indent .. "}")
+    else
+      print(indent .. k .. " = " .. tostring(v))
+    end
   end
 end
 gd.print_table = print_table
@@ -162,10 +173,21 @@ function gd.resource_load(file_path)
   end
   local resource = {}
   for line in file:lines() do
-    local section = line:match(section_pattern)
-    if section then
-      -- TODO section attrs!
-      resource[section] = parse_section(file)
+    local header = line:match(header_pattern)
+    if header then
+      local tag, attrs = parse_header(trim(header))
+      local key
+      if tag == "gd_resource" or tag == "gd_scene" then
+        resource.uid = attrs.uid
+        key = tag
+      else
+        key = attrs.id or attrs.name or tag
+      end
+      resource[key] = {
+        tag = tag,
+        attrs = attrs,
+        body = parse_section(file),
+      }
     end
   end
   return resource
