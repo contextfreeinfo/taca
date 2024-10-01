e7376186fee7eeb2ba80772fa8546f77f3a01b1a
Tom <tom@contextfree.info>
2024-09-30 18:43:11 -0700

    Rename main; clear old comments

--- examples/nelua/walk/build.sh
+++ examples/nelua/walk/build.sh
@@ -4,7 +4,7 @@ export WASI_SDK=$HOME/apps/wasi-sdk
 
 naga src/shader.wgsl out/shader.spv && \
 nelua --cc="$WASI_SDK/bin/clang" --add-path src -o out/walk.wasm --release \
-    src/walk.nelua && \
+    src/main.nelua && \
 wasm2wat --generate-names out/walk.wasm -o out/walk.wat && \
 lz4 -f9 out/walk.wasm out/walk.taca && \
 mkdir -p $PUB_DIR && \
