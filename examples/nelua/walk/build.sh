0eb9c7d9a1848a97340731411da1c50e8ea2c618
Tom <tom@contextfree.info>
2024-10-08 07:56:23 -0700

    Skip wasm2wat

--- examples/nelua/walk/build.sh
+++ examples/nelua/walk/build.sh
@@ -5,10 +5,12 @@ mkdir -p out && \
 naga src/shader.wgsl out/shader.spv && \
 nelua --cc="$WASI_SDK/bin/clang" --add-path src -o out/walk.wasm --release \
     src/main.nelua && \
-wasm2wat --generate-names out/walk.wasm -o out/walk.wat && \
 lz4 -f9 out/walk.wasm out/walk.taca && \
 mkdir -p $PUB_DIR && \
 cp out/walk.taca $PUB_DIR/
+
+# Inspection
+# wasm2wat --generate-names out/walk.wasm -o out/walk.wat && \
 
 # Only saving a tiny bit here:
 # wasm-opt -Os out/walk.wasm -o out/walk.opt.wasm && \
