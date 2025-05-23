5719580c6f9806b9792d73545e7914749aff03a6
Tom <tom@contextfree.info>
2025-05-22 18:35:24 -0700

    Call both c and rust

--- native/src/wasm.rs
+++ native/src/wasm.rs
@@ -42,10 +42,17 @@ pub fn run() -> Result<()> {
     let mut linker = Linker::new(&engine);
     console::add_to_linker(&mut linker, |state: &mut MyState| &mut state.host)?;
     key::add_to_linker(&mut linker, |state: &mut MyState| &mut state.host)?;
-    let component: &[u8] = include_bytes!("../../examples/c/hi/out/hi-component.wasm");
-    let component = Component::from_binary(&engine, &component)?;
-    let taca = Taca::instantiate(&mut store, &component, &linker)?;
-    let app = taca.taca_core_app();
-    app.call_update(&mut store, app::EventKind::Frame)?;
+    let component_c: &[u8] = include_bytes!("../../examples/c/hi/out/hi-component.wasm");
+    let component_c = Component::from_binary(&engine, &component_c)?;
+    let taca_c = Taca::instantiate(&mut store, &component_c, &linker)?;
+    let app_c = taca_c.taca_core_app();
+    let component_rust: &[u8] = include_bytes!(
+        "../../examples/rust/hi/target/wasm32-unknown-unknown/release/hi-component.wasm"
+    );
+    let component_rust = Component::from_binary(&engine, &component_rust)?;
+    let taca_rust = Taca::instantiate(&mut store, &component_rust, &linker)?;
+    let app_rust = taca_rust.taca_core_app();
+    app_c.call_update(&mut store, app::EventKind::Frame)?;
+    app_rust.call_update(&mut store, app::EventKind::Frame)?;
     Ok(())
 }
