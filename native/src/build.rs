7c365bf38c05c1b0b8f7e30b8c18bc5b49b3e6da
Tom <tom@contextfree.info>
2025-05-26 04:51:17 -0700

    Organize better and timestamp seal

--- native/src/build.rs
+++ native/src/build.rs
@@ -2,12 +2,12 @@ use crate::Cli;
 use anyhow::Result;
 use base64::engine::{Engine, general_purpose};
 use ed25519_dalek::SigningKey;
+use jiff::{Timestamp, TimestampRound, Unit};
 use rand::rngs::OsRng;
-use serde::Serialize;
-use serde_json::{Value, json};
+use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha512};
 use std::fs::{self, File};
-use std::io::{Cursor, Read, Seek, Write};
+use std::io::{Read, Seek, Write};
 use std::path::Path;
 use zip::{ZipWriter, write::SimpleFileOptions};
 
@@ -15,6 +15,7 @@ struct Bundler<'a> {
     entries: Vec<Entry>,
     root: &'a Path,
     signing_key: &'a SigningKey,
+    signing_key_created_at: Timestamp,
 }
 
 #[derive(Debug)]
@@ -33,14 +34,16 @@ fn bundle(src: &Path) -> Result<()> {
     let zip_file = File::create(dest)?;
     let mut zip = ZipWriter::new(zip_file);
     let mut secure_rng = OsRng;
+    // TODO Reuse or store key in private file, with created timestamp.
     let signing_key = SigningKey::generate(&mut secure_rng);
-    // TODO Reuse or store key in private file.
-    // TODO Store creation date with key, and store in app.json.
+    let signing_key_created_at =
+        Timestamp::now().round(TimestampRound::new().smallest(Unit::Millisecond))?;
     // TODO Option to export/import key with strength-checked passphrase.
     let mut bundler = Bundler {
         entries: vec![],
         root: src,
         signing_key: &signing_key,
+        signing_key_created_at,
     };
     // TODO Verify required contents.
     add_dir_to_zip(&mut bundler, &mut zip, src)?;
@@ -60,9 +63,9 @@ fn add_dir_to_zip<W: Seek + Write>(
         let path = entry.path();
         let name = path.strip_prefix(bundler.root).unwrap().to_str().unwrap();
         match name {
-            "app.json" => {
-                add_pubkey_to_json(bundler, zip, &path)?;
-            }
+            // "app.json" => {
+            //     add_pubkey_to_json(bundler, zip, &path)?;
+            // }
             _ if path.is_file() => {
                 // TODO Specially process some files like app.json.
                 // TODO Automate things like public key.
@@ -81,24 +84,25 @@ fn add_dir_to_zip<W: Seek + Write>(
     Ok(())
 }
 
-fn add_pubkey_to_json<W: Seek + Write>(
-    bundler: &mut Bundler,
-    zip: &mut ZipWriter<W>,
-    path: &Path,
-) -> Result<()> {
-    let json_str = fs::read_to_string(path)?;
-    let mut value: Value = serde_json::from_str(&json_str)?;
-    let pubkey_bytes = bundler.signing_key.verifying_key().to_bytes();
-    let pubkey_b64 = general_purpose::STANDARD.encode(pubkey_bytes);
-    // TODO Also key creation date?
-    if let Value::Object(map) = &mut value {
-        map.insert("publicKey".to_string(), json!(pubkey_b64));
-    }
-    let content = serde_json::to_string_pretty(&value)?;
-    let cursor = Cursor::new(content.as_bytes());
-    add_signed_file(bundler, zip, "app.json", cursor)?;
-    Ok(())
-}
+// fn add_pubkey_to_json<W: Seek + Write>(
+//     bundler: &mut Bundler,
+//     zip: &mut ZipWriter<W>,
+//     path: &Path,
+// ) -> Result<()> {
+//     let json_str = fs::read_to_string(path)?;
+//     let mut value: Value = serde_json::from_str(&json_str)?;
+//     let pubkey_bytes = bundler.signing_key.verifying_key().to_bytes();
+//     let pubkey_b64 = general_purpose::STANDARD.encode(pubkey_bytes);
+//     // TODO Also key creation date?
+//     // TODO As sub object with value, createdAt, and maybe algo?
+//     if let Value::Object(map) = &mut value {
+//         map.insert("publicKey".to_string(), json!(pubkey_b64));
+//     }
+//     let content = serde_json::to_string_pretty(&value)?;
+//     let cursor = Cursor::new(content.as_bytes());
+//     add_signed_file(bundler, zip, "app.json", cursor)?;
+//     Ok(())
+// }
 
 fn add_signed_file<R: Read, W: Seek + Write>(
     bundler: &mut Bundler,
@@ -142,18 +146,45 @@ fn zip_options() -> SimpleFileOptions {
 //     fs::write(path, key.to_bytes())
 // }
 
-#[derive(Serialize)]
-struct JsonEntry(String, String);
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Seal {
+    sealed_at: Timestamp,
+    key: SealKeyInfo,
+    entries: Vec<SealEntry>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct SealEntry(String, String);
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct SealKeyInfo {
+    created_at: Timestamp,
+    public: String,
+}
 
 fn write_list(bundler: &Bundler, out: &mut impl Write) -> Result<()> {
-    for entry in &bundler.entries {
-        let json_entry = JsonEntry(
-            entry.name.clone(),
-            general_purpose::STANDARD.encode(&entry.hash),
-        );
-        let line = serde_json::to_string(&json_entry).unwrap();
-        writeln!(out, "{}", line)?;
-    }
+    let public = bundler.signing_key.verifying_key().to_bytes();
+    let public = general_purpose::STANDARD.encode(public);
+    let seal = Seal {
+        sealed_at: Timestamp::now().round(TimestampRound::new().smallest(Unit::Millisecond))?,
+        key: SealKeyInfo {
+            created_at: bundler.signing_key_created_at,
+            public,
+        },
+        entries: bundler
+            .entries
+            .iter()
+            .map(|entry| {
+                SealEntry(
+                    entry.name.clone(),
+                    general_purpose::STANDARD.encode(&entry.hash),
+                )
+            })
+            .collect(),
+    };
+    serde_json::to_writer_pretty(out, &seal)?;
     Ok(())
 }
 
@@ -161,7 +192,7 @@ fn write_list_signed<W: Seek + Write>(
     bundler: &Bundler,
     zip: &mut zip::ZipWriter<W>,
 ) -> Result<()> {
-    zip.start_file("list.jsonl", zip_options())?;
+    zip.start_file("seal.json", zip_options())?;
     let mut hasher = Sha512::new();
     let mut tee = HashTee {
         writer: zip,
@@ -171,7 +202,7 @@ fn write_list_signed<W: Seek + Write>(
     let sig = bundler
         .signing_key
         .sign_prehashed(hasher, Some(TACA_RUNTIME_SIGNING_CONTEXT))?;
-    zip.start_file("list.sig", zip_options())?;
+    zip.start_file("seal.sig", zip_options())?;
     writeln!(zip, "{}", general_purpose::STANDARD.encode(sig.to_bytes()))?;
     Ok(())
 }
